36ba46abda3d231c4dc90285174eda95786540f3
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-01-18 06:10:43 +0700

    Menambahkan list display pada model event dan event_participant

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Event, Location
-admin.site.register(Event)
+from .models import Event, Location, Event_participant
+
+class Event_participantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'role')
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event_type', 'date', 'time')
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(Location)
+admin.site.register(Event_participant, Event_participantAdmin)
