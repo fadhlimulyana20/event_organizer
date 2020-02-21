30cf26818ce283faf41568268822ff808ecfb4c7
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-02-21 19:12:44 +0700

    system path untuk server pada wsgi.py

--- user_regist/wsgi.py
+++ user_regist/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_regist.settings')
+sys.path.append('/var/www//django-apps/source/event_organizer')
+sys.path.append('/var/www//django-apps/source/event_organizer/user_regist')
 
 application = get_wsgi_application()
