568d218da1463ee98077486c42d3a22aa4771a73
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-02-28 10:13:21 +0700

    menambahkan static untuk admin pada setting

--- user_regist/settings.py
+++ user_regist/settings.py
@@ -138,12 +138,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
+    '/var/www//django-apps/env/lib/python3.6/site-packages/static/admin',
 ]
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 
 #media file configuration
