a36eeb7dd1b9fd34c5298a386fedfd5471e39967
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-02-28 10:06:12 +0700

    menambahkan static admin media pada setting

--- user_regist/settings.py
+++ user_regist/settings.py
@@ -140,6 +140,12 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+
 #media file configuration
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
