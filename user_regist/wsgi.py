d66067c3aa8d637ff8d5da9a284053093a194f55
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-02-28 10:02:28 +0700

    menambahkan path pada wsgi untuk apache2

--- user_regist/wsgi.py
+++ user_regist/wsgi.py
@@ -13,7 +13,18 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_regist.settings')
-sys.path.append('/var/www//django-apps/source/event_organizer')
-sys.path.append('/var/www//django-apps/source/event_organizer/user_regist')
+# sys.path.append('/var/www//django-apps/source/event_organizer')
+# sys.path.append('/var/www//django-apps/source/event_organizer/user_regist')
+
+paths = [
+    '/var/www//django-apps/source/event_organizer',
+    '/var/www//django-apps/source/event_organizer/user_regist',
+    '/var/www//django-apps/env/lib/python3.6/site-packages',
+]
+
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
 
 application = get_wsgi_application()
