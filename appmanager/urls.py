233479c9c55aeec94b7ed91e42d9e7489e885698
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-03-02 05:13:40 +0700

    Seccond Commit

--- appmanager/urls.py
+++ appmanager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import dash_admin_view
+from .views import dash_admin_view, user_admin_view
 app_name = 'appmanager'
 
 urlpatterns = [
-    path('', dash_admin_view, name='dash_admin_view')
+    path('', dash_admin_view, name='dash_admin_view'),
+    path('user/', user_admin_view, name='user_admin_view')
 ]
--- appmanager/views.py
+++ appmanager/views.py
@@ -1,6 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+from payment.models import Invoice
+from ticketing.models import EventParticipant
+from django.core.paginator import Paginator
+
 
 # Create your views here.
+def superuser_check(user):
+    return user.is_superuser
+
+@user_passes_test(superuser_check)
 def dash_admin_view(request):
-    return HttpResponse('good')
+    user = User.objects.all()
+    invoice = Invoice.objects.filter(pay_status=True)
+    invoice_debt = Invoice.objects.filter(pay_status=False)
+
+    get_income = 0
+    for instance in invoice:
+        get_income += instance.price
+
+    context = {
+        'user' : user,
+        'invoice' : invoice,
+        'invoice_debt' : invoice_debt,
+        'get_income' : get_income
+    }
+    return render(request, 'manager_home.html', context)
+
+@user_passes_test(superuser_check)
+def user_admin_view(request):
+    user = User.objects.all()
+    paginator = Paginator(user, 25) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj' : page_obj
+    }
+
+    return render(request, 'user_management.html', context)
+
+@user_passes_test(superuser_check)
+def ep_admin_view(request):
+    event_participant = EventParticipant.objects.all()
+    paginator = Paginator(event_participant, 25)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj' : page_obj
+    }
+
+    return render(request, 'ep_management.html', context)
